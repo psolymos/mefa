a9e39491e708083de0e57d7749e6da0529d856a0
psolymos <psolymos@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-03-14 11:40:52 +0000

    minor changesand fixfor add.attrib

--- pkg/R/add.attrib.R
+++ pkg/R/add.attrib.R
@@ -1,27 +1,26 @@
 `add.attrib` <-
-function(mf, which=c("samples", "species"), attrib, index=0){
-
-if(class(mf) != "mefa") stop("Object '",mf,"' is not of 'mefa' class.")
-
-xord <- xorder(as.xcount(mf$data), which, attrib, index)
-
-if(index != 0) xord$data[[index]] <- NULL
-
-if(which == "species") {
-	xsamp.out <- mf$sample.attr
-	xspec.out <- data.frame(mf$species.attr, xord$data)
-	nsample.attr <- mf$nsample.attr
-	nspecies.attr <- dim(xspec.out)[2]}
-
-if(which == "samples") {
-	xsamp.out <- data.frame(mf$sample.attr, xord$data)
-	xspec.out <- mf$species.attr
-	nsample.attr <- dim(xsamp.out)[2]
-	nspecies.attr <- mf$nspecies.attr}
-
-xco <- as.xcount(mf$data)
-xo1 <- xorder(xco, "samples", xsamp.out)
-xo2 <- xorder(xco, "species", xspec.out)
-out <- mefa(xco, xo1, xo2)
-return(out)}
-
+function (mf, which = c("samples", "species"), attrib, index = 0) 
+{
+    if (class(mf) != "mefa") 
+        stop("Object '", mf, "' is not of 'mefa' class.")
+    xord <- xorder(as.xcount(mf$data), which, attrib, index)
+    if (index != 0) 
+        xord$data[[index]] <- NULL
+    if (which == "species") {
+        xsamp.out <- mf$sample.attr
+        if(is.null(mf$species.attr)) {xspec.out <- as.data.frame(xord$data)
+            } else {
+            xspec.out <- data.frame(mf$species.attr, xord$data)}
+    }
+    if (which == "samples") {
+        xspec.out <- mf$species.attr
+        if(is.null(mf$sample.attr)) {xsamp.out <- as.data.frame(xord$data)
+            } else {
+            xsamp.out <- data.frame(mf$sample.attr, xord$data)}
+    }
+    xco <- as.xcount(mf$data)
+    if(is.null(xsamp.out)) xo1 <- NULL else xo1 <- xorder(xco, "samples", xsamp.out)
+    if(is.null(xspec.out)) xo2 <- NULL else xo2 <- xorder(xco, "species", xspec.out)
+    out <- mefa(xco, xo1, xo2)
+    return(out)
+}
