693f02d42adfb571576681e204b90d5f8d33f384
psolymos <psolymos@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-04-03 07:49:30 +0000

    accumulate extended

--- pkg/R/accumulate.R
+++ pkg/R/accumulate.R
@@ -1,30 +1,33 @@
 `accumulate` <-
-function(x){
 
-if (class(x) != "sscount") 
-        stop("Object '", x, "' is not of 'sscount' class.")
+function(x){
 
 tabl <- x$data[order(x$data$segment),]
 nyr <- length(levels(tabl$segment))
 
-nrecord <- nspec <- newspec <- rep(0, nyr)
+nsamp <- newsamp <- nrecord <- nspec <- newspec <- rep(0, nyr)
 first <- tabl[which(tabl$segment == levels(tabl$segment)[1]),]
 first[] <- lapply(first, function(x) x[drop = TRUE])
 nrecord[1] <- sum(first$count)
 nspec[1] <- newspec[1] <- nrow(first)
-oldlist <- first$species
+oldlist1 <- first$species
+oldlist2 <- first$sample
 
 for (i in 2:nyr){
     second <- tabl[which(tabl$segment == levels(tabl$segment)[i]),]
     second[] <- lapply(second, function(x) x[drop = TRUE])
     nrecord[i] <- sum(second$count)
-    newlist <- union(oldlist, levels(second$species))
-    nspec[i] <- nlevels(as.factor(newlist))
+    newlist1 <- union(oldlist1, levels(second$species))
+    nspec[i] <- nlevels(as.factor(newlist1))
     newspec[i] <- nspec[i] - nspec[(i-1)]
-    oldlist <- newlist
+    oldlist1 <- newlist1
+    newlist2 <- union(oldlist2, levels(second$sample))
+    nsamp[i] <- nlevels(as.factor(newlist2))
+    newsamp[i] <- nsamp[i] - nsamp[(i-1)]
+    oldlist2 <- newlist2
     }
 
-out <- data.frame(as.numeric(levels(tabl$segment)), nrecord, newspec, cumsum(nrecord), nspec)
-colnames(out) <- c("segment", "newrecord", "newspec", "cumrecord", "cumspec")
+out <- data.frame(as.numeric(levels(tabl$segment)), nrecord, newsamp, newspec, cumsum(nrecord), nsamp, nspec)
+colnames(out) <- c("segment", "newrecord", "newsamp", "newspec", "cumrecord", "cumsamp", "cumspec")
 return(out)}
 
