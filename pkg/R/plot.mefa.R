a2cc9a62ed4ea93905ac3ccdc1bbb67308e37d15
psolymos <psolymos@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-09-20 02:00:42 +0000

    methods improves, report added

--- pkg/R/plot.mefa.R
+++ pkg/R/plot.mefa.R
@@ -1,25 +1,59 @@
 `plot.mefa` <-
-function(x, which=1:4, ylab=NULL, xlab=NULL, ...)
+function(x, which=1:4, type=c("hist", "rank"), trafo=c("none", "log",
+"ratio"), ylab=NULL, xlab=NULL, plot=TRUE, ...)
 {
-    if (!all(which %in% 1:4))
-        stop("which must be in 1:4")
-    if (!length(which) == 1) which <- 1
-    if (which == 1) {
-        if (is.null(ylab)) ylab <- "Frequency (samples)"
-        if (is.null(xlab)) xlab <- "Number of taxa"
-        plot(table(summary(x)$srich), xlab=xlab, ylab=ylab, ...)}
-    if (which == 2) {
-        if (is.null(ylab)) ylab <- "Frequency (samples)"
-        if (is.null(xlab)) xlab <- "Number of individuals"
-        plot(table(summary(x)$srich), xlab=xlab, ylab=ylab, ...)}
-    if (which == 3) {
-        if (is.null(ylab)) ylab <- "Frequency (taxa)"
-        if (is.null(xlab)) xlab <- "Occupancy"
-        plot(table(summary(x)$spocc), xlab=xlab, ylab=ylab, ...)}
-    if (which == 4) {
-        if (is.null(ylab)) ylab <- "Frequency (taxa)"
-        if (is.null(xlab)) xlab <- "Abundance"
-        plot(table(summary(x)$spabu), xlab=xlab, ylab=ylab, ...)}
-    invisible()
-}
+   if (!all(which %in% 1:4))
+       stop("which must be in 1:4")
+   if (!length(which) == 1) which <- 1
+   if (!length(type) == 1) type <- type[1]
+   if (!length(trafo) == 1) trafo <- trafo[1]
+   type <- match.arg(type, c("hist", "rank"))
+   trafo <- match.arg(trafo, c("none", "log", "ratio"))
 
+   if (!is.null(ylab))
+       ylab2 <- ylab
+   if (!is.null(xlab))
+       xlab2 <- xlab
+   if (is.null(ylab) && type=="hist")
+       ylab2 <- "Frequency"
+   if (is.null(ylab) && type=="rank")
+       ylab2 <- "Rank"
+
+   if (which == 1) {
+       if (is.null(ylab)) ylab2 <- paste(ylab2, "(samples)")
+       if (is.null(xlab)) xlab2 <- "Number of taxa"
+       yvar <- summary(x)$srich}
+   if (which == 2) {
+       if (is.null(ylab)) ylab2 <- paste(ylab2, "(samples)")
+       if (is.null(xlab)) xlab2 <- "Number of individuals"
+       yvar <- summary(x)$ninds}
+   if (which == 3) {
+       if (is.null(ylab)) ylab2 <- paste(ylab2, "(taxa)")
+       if (is.null(xlab)) xlab2 <- "Occupancy"
+       yvar <- summary(x)$spocc}
+   if (which == 4) {
+       if (is.null(ylab)) ylab2 <- paste(ylab2, "(taxa)")
+       if (is.null(xlab)) xlab2 <- "Abundance"
+       yvar <- summary(x)$spabu}
+
+   if (trafo=="log") {
+       yvar <- log10(yvar)
+       if (is.null(ylab) && type=="hist")
+           ylab <- paste("log10", ylab2)
+       if (is.null(ylab) && type=="rank")
+           xlab <- paste("log10", xlab2)}
+   if (trafo=="ratio") {
+       yvar <- yvar / max(yvar)
+       if (is.null(ylab) && type=="hist")
+           ylab <- paste("Relative", tolower(ylab2))
+       if (is.null(ylab) && type=="rank")
+           xlab <- paste("Relative", tolower(xlab2))}
+   if (plot) {
+       if (type=="hist")
+           plot(table(yvar), xlab=xlab2, ylab=ylab2, ...)
+       if (type=="rank")
+           plot(yvar[order(yvar, decreasing=TRUE)], type="l",
+xlab=ylab2, ylab=xlab2, ...)
+       }
+   invisible(yvar)
+}
