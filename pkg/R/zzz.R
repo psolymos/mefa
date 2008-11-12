9928891ce9fa4cf4bec393417711b2bf355cea60
psolymos <psolymos@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-11-12 07:35:37 +0000

    2.1 finished -- need some testing before CRAN submission

--- pkg/R/zzz.R
+++ pkg/R/zzz.R
@@ -1,3 +1,4 @@
 .First.lib <- function(lib, pkg){
-    cat("This is mefa", utils::packageDescription("mefa", field="Version"), "\n")
+    cat("This is mefa ", utils::packageDescription("mefa", field="Version"),
+    " (", utils::packageDescription("mefa", field="Date"), ")\n", sep="")
 }
