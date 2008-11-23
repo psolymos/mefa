1ab6f29b4545391141a3de8140714c100a080ca1
psolymos <psolymos@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-11-23 00:18:45 +0000

    Rd files

--- pkg/abmi/R/separateint.R
+++ pkg/abmi/R/separateint.R
@@ -1,9 +1,12 @@
 ## separates interaction vector
 separateint <-
-function(x, step = 0, spacer = 1)
+function(x, step = 1, spacer = 1)
 {
+    if (is.list(x))
+        stop("'x' must not be list")
     x <- as.character(x)
     n <- nchar(x)
+    step <- step - 1
     first <- substr(x, 1, n-step-1-spacer)
     second <- substr(x, n-step, n)
     out <- cbind(first, second)
