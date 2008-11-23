26092126c492628d14f204b7d08e6ec2842fbbf8
psolymos <psolymos@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-11-23 02:07:59 +0000

    check o.k.

--- pkg/abmi/R/lincovtest.R
+++ pkg/abmi/R/lincovtest.R
@@ -1,10 +1,18 @@
 ## tests linear relationship for binomial GLM
 lincovtest <-
-function(y, x, probs = seq(0, 1, 0.25), na.rm = FALSE, ...)
+function(y, x, probs = seq(0, 1, 0.25), na.rm = FALSE)
 {
     require(mefa)
-    q <- qvector(x, probs = seq(0, 1, 0.25), na.rm = na.rm, ...)
+    noNA <- which(apply(is.na(cbind(y,x)), 1, sum) == 0)
+    if (!na.rm && length(noNA) != length(y))
+        stop("NA values not allowed")
+    if (na.rm) {
+        y <- y[noNA]
+        x <- x[noNA]
+    }
+    q <- qvector(x, probs = seq(0, 1, 0.25))
     z <- as.factor(q)
+    y <- ifelse(y > 0, 1, 0)
     m <- coef(glm(y ~ z, family=binomial))
     names(m) <- levels(z)
     class(m) <- c("lincovtest", class(m))
