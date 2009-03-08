74d39c5404a83c612e003e8aa7c1a03a29f729ae
psolymos <psolymos@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-03-08 08:09:06 +0000

    sandbox update

--- dev/sandbox.R
+++ dev/sandbox.R
@@ -1,3 +1,90 @@
+library(mefa)
+data(dol.count)
+ss <- stcs(dol.count)
+x <- mefa(ss)
+
+## as.* methods
+
+`as.matrix.mefa` <-
+function(x, ...)
+{
+    x$xtab
+}
+
+`as.list.mefa` <-
+function(x, ...)
+{
+    if (is.null(x$segm) || length(x$segm) == 1) {
+        return(list(x$xtab))
+    } else {
+        return(x$segm)
+    }
+}
+
+`as.array.mefa` <-
+function(x, ...)
+{
+    DIM <- dim(x)
+    DIMNAMES <- dimnames(x)
+    DATA <- unlist(as.list.mefa(x))
+    array(DATA, DIM, DIMNAMES)
+}
+
+`as.data.frame.stcs` <-
+function(x, ...)
+{
+    attr(x, "call") <- NULL
+    attr(x, "expand") <- NULL
+    attr(x, "zero.count") <- NULL
+    attr(x, "zero.pseudo") <- NULL
+    class(x) <- "data.frame"
+    x
+}
+
+`as.mefa.list` <-
+function(x, nested = FALSE, ...)
+{
+    n <- length(x)
+    if (n == 1 && is.matrix(x[[1]]))
+        return(as.mefa.default(x[[1]]))
+    tmp <- x
+    for (i in 2:n) {
+        tmp[[i]] <- tmp[[(i-1)]] + x[[i]]
+    }
+    m <- mefa(tmp[[n]])
+    if (nested) {
+        m$segm <- x
+        attr(m, "nested") <- TRUE
+    } else {
+        m$segm <- tmp
+    }
+    m
+}
+
+`as.mefa.array` <-
+function(x, nested = FALSE, ...)
+{
+    if (length(dim(x)) == 2)
+        return(as.mefa.default(x[[1]]))
+    n <- dim(x)[3]
+    tmp <- list()
+    tmp[[1]] <- x[,,1]
+    for (i in 2:n) {
+        tmp[[i]] <- tmp[[(i-1)]] + x[,,i]
+    }
+    m <- mefa(tmp[[n]])
+    if (nested) {
+        m$segm <- x
+        attr(m, "nested") <- TRUE
+    } else {
+        names(tmp) <- dimnames(x)[[3]]
+        m$segm <- tmp
+    }
+    m
+}
+
+##
+
 nestedboard <-
 function(comm, board = "B")
 {
