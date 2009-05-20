22497079702a66a7ca081fa321f00d456c9216d2
psolymos <psolymos@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-05-20 14:56:02 +0000

    match.fun

--- pkg/mefa/R/as.data.frame.mefa.R
+++ pkg/mefa/R/as.data.frame.mefa.R
@@ -1,6 +1,7 @@
 as.data.frame.mefa <-
 function(x, ..., fun, name, make.unique = FALSE)
 {
+    fun <- match.fun(fun)
     if (!inherits(x, "mefa"))
         stop("'x' must be of class mefa")
     if (missing(fun))
