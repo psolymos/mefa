a1701e1ac1786fcd9bc1b4f288638a4066a990ee
psolymos <psolymos@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-05-26 22:12:40 +0000

    select arg in glmexplorer

--- pkg/dclone/R/glmexplorer.R
+++ pkg/dclone/R/glmexplorer.R
@@ -1,5 +1,5 @@
 glmexplorer <-
-function(formula, data, family=poisson(), ...)
+function(formula, data, family=poisson(), select=FALSE, ...)
 {
     lhs <- formula[[2]]
     if (missing(data))
@@ -14,12 +14,19 @@ function(formula, data, family=poisson(), ...)
         stop("'NA' values not allowed")
     n <- NROW(Y)
     J <- NCOL(Y)
-    glmfit <- lapply(1:J, function(z) glm.fit(X, Y[, z], family=family, ...))
-    glmsum <- lapply(glmfit, function(z) {
-        class(z) <- "glm"
-        summary(z)$coef})
-    names(glmsum) <- colnames(Y)
-    class(glmsum) <- "glmexplorer"
-    glmsum
+    if (select) {
+        glmfit <- lapply(1:J, function(z) glm(Y[, z] ~ ., rhs, family=family))
+        glmsel <- lapply(glmfit, function(z) step(z, direction="backward", trace=0))
+        rval <- lapply(glmsel, function(z) summary(z)$coef)
+    } else {
+        glmfit <- lapply(1:J, function(z) glm.fit(X, Y[, z], family=family))
+        rval <- lapply(glmfit, function(z) {
+            class(z) <- "glm"
+            summary(z)$coef})
+    }
+    names(rval) <- colnames(Y)
+    class(rval) <- "glmexplorer"
+    attr(rval, "family") <- family$family
+    attr(rval, "select") <- select
+    rval
 }
-
--- pkg/dclone/R/summary.glmexplorer.R
+++ pkg/dclone/R/summary.glmexplorer.R
@@ -1,14 +1,23 @@
 summary.glmexplorer <-
 function(object, crit=0.05, ...)
 {
-    rval <- as.data.frame(lapply(object, function(z) z[,1]))
-    rownames(rval) <- rownames(object[[1]])
-    pval <- as.data.frame(lapply(object, function(z) z[,4]))
-    rval <- data.matrix(rval)
-    pval <- data.matrix(pval)
-    if (rownames(object[[1]])[1] == "(Intercept)")
-        pval[1,] <- 0
-    rval[pval >= crit] <- NA
+    if (attr(object, "select")) {
+        est <- lapply(object, function(z) z[,1])
+        nam <- unique(unlist(lapply(object, rownames)))
+        rval <- matrix(NA, length(nam), length(est),
+            dimnames=list(nam, names(est)))
+        for (i in 1:length(est)) {
+            rval[rownames(object[[i]]),i] <- est[[i]]
+        }
+    } else {
+        rval <- as.data.frame(lapply(object, function(z) z[,1]))
+        rownames(rval) <- rownames(object[[1]])
+        pval <- as.data.frame(lapply(object, function(z) z[,4]))
+        rval <- data.matrix(rval)
+        pval <- data.matrix(pval)
+        if (rownames(object[[1]])[1] == "(Intercept)")
+            pval[1,] <- 0
+        rval[pval >= crit] <- NA
+    }
     rval
 }
-
