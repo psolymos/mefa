2ba6dab6bab094121ab85d07428478ed020d7886
psolymos <psolymos@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-06-09 15:55:18 +0000

    update

--- pkg/mefa/R/as.data.frame.dist.R
+++ pkg/mefa/R/as.data.frame.dist.R
@@ -11,8 +11,7 @@ function (x, row.names = NULL, optional = FALSE, ...)
     cm <- col(id)
     rm <- array(rm)[array(id) == 1]
     cm <- array(cm)[array(id) == 1]
-    attributes(x) <- NULL
-    out <- data.frame(row=rm, col=cm, dist=x)
+    out <- data.frame(row=rm, col=cm, dist=dist2vec(x))
     if (!is.null(row.names))
         rownames(out) <- row.names
     out
