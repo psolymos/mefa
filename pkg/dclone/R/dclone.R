aa904db147127500c6a9dd514b894b99c591e281
psolymos <psolymos@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-07-11 00:05:09 +0000

    dclone is generic

--- pkg/dclone/R/dclone.R
+++ pkg/dclone/R/dclone.R
@@ -1,32 +1,3 @@
 dclone <-
-function(x, n.clones=1)
-{
-    if (n.clones==1) {
-        rval <- x
-    } else {
-        if (!is.null(dim(x)) || is.list(x)) {
-            NAMES <- row.names(x)
-            rval <- lapply(as.data.frame(x), function(z) rep(z, n.clones))
-            rval <- as.data.frame(rval)
-            if (!is.null(NAMES))
-                row.names(rval) <- paste(NAMES, rep(1:n.clones, each=length(NAMES)), sep="_")
-            if (is.matrix(x)) {
-                rval <- as.matrix(rval)
-                colnames(rval) <- colnames(x)
-            }
-            if (is.list(x) && !is.data.frame(x)) {
-                rval <- as.list(rval)
-                attr(rval,"row.names") <- NULL
-            }
-        } else {
-            NAMES <- names(x)
-            rval <- rep(array(x), n.clones)
-            if (!is.null(NAMES))
-                names(rval) <- paste(NAMES, rep(1:n.clones, each=length(NAMES)), sep="_")
-        }
-    }
-    class(rval) <- c("dclone", class(x))
-    attr(rval, "n.clones") <- n.clones
-    rval
-}
-
+function(x, ...)
+UseMethod("dclone")
