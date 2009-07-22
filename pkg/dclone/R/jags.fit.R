1801b429561d5fa680770458beee3e5d838cd458
psolymos <psolymos@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-07-22 04:37:58 +0000

    suggested changes in jags.fit

--- pkg/dclone/R/jags.fit.R
+++ pkg/dclone/R/jags.fit.R
@@ -2,6 +2,10 @@ jags.fit <-
 function(data, params, model, n.chains=3, n.adapt=1000, n.update=0, thin=1, n.iter=5000, progress.bar="text", ...)
 {
     n.clones <- nclones.list(data)
+    if (is.function(model)) {
+        model <- write.jags.model(jfun)
+        on.exit(clean.jags.model(model))
+    }
     m <- jags.model(model, data, n.chain=n.chains, n.adapt=n.adapt, ...)
     if (n.update > 0) {
         update(m, n.update, progress.bar=progress.bar)
