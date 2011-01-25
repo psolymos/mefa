5b0e01b835a2b4255cd568be7ed93eb8f39315c2
psolymos <psolymos@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-01-25 06:49:34 +0000

    updated todo list

--- dev/mefa4.R
+++ dev/mefa4.R
@@ -14,16 +14,21 @@ OK - taxa, taxa<-
 OK - [<-
 OK - coercion methods between S3 and S4 classes (went to mefa)
 OK - coercion between Xtab and Mefa
+OK - aggregate -- no sparse implementation, use aggregate(as.mefa), ...)
+     but that is sloooow. An implementation is ready in my Dropbox folder
+     it is implemented as groupSums and groupMeans methods
+OK - show
+OK - test directory with example and performance comparison code
 
-- show, print, summary
+- Xtab methods for [ and [<- (different siglists?), now returns dgCMatrix
+
+- print, summary (not sure what to do with these)
 - plot, image (see image plot for Matrix classes) - it is not urgent, image(as.mefa(x))
-- aggregate -- no sparse implementation, use aggregate(as.mefa), ...)
-  but that is sloooow. An implementation is ready in my Dropbox folder
 ? set a class union for XtabMatrix and Mefa -- currently no need for it
 ? which operatons should affect @call? -- update all the time, or remove @call
 ? is there a need for call and update? -- I think no
 
 - join 2 Xtab, Mefa objects, inner/outer/left/right join operations
-  needs careful planning, but should be pretty straightforward
+     needs careful planning, but should be pretty straightforward
 
 - add a vignette on performance comparisons, new classes and methods
