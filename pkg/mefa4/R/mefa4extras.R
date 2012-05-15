35dda563ec314c9027f1462c047e8fd3abbccf68
psolymos <psolymos@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-05-15 04:48:15 +0000

    mbind2 for summed x and y

--- pkg/mefa4/R/mefa4extras.R
+++ pkg/mefa4/R/mefa4extras.R
@@ -281,3 +281,39 @@ setMethod("mbind", signature(x="Mefa", y="Mefa", fill="ANY"),
         ## assembling
         Mefa(part5, sm2, tm2, join="left", drop)
 })
+
+setGeneric("mbind2", function(x, y, fill, ...) standardGeneric("mbind2"))
+
+setMethod("mbind2", signature(x="matrix", y="matrix", fill="ANY"), 
+    function(x, y, fill, ...) {
+        as.matrix(mbind2(x=as(x, "sparseMatrix"),
+            y=as(y, "sparseMatrix"),
+            fill=fill, ...))
+})
+
+setMethod("mbind2", signature(x="sparseMatrix", y="sparseMatrix", fill="ANY"), 
+    function(x, y, fill, ...) {
+        if (missing(fill))
+            fill <- NA
+        if (length(x) == 0)
+            stop("length of 'x' must not be 0")
+        if (length(y) == 0)
+            stop("length of 'y' must not be 0")
+        if (is.null(dimnames(x)))
+            stop("dimnames of 'x' must not be NULL")
+        if (is.null(dimnames(y)))
+            stop("dimnames of 'y' must not be NULL")
+        rn <- union(rownames(x), rownames(y))
+        cn <- union(colnames(x), colnames(y))
+        rn0 <- intersect(rownames(x), rownames(y))
+        cn0 <- intersect(colnames(x), colnames(y))
+        out <- Matrix(fill, 
+            length(rn), length(cn), 
+            dimnames=list(rn, cn),
+            sparse=TRUE)
+        out <- as(out, "dgCMatrix")
+        out[rownames(x), colnames(x)] <- x
+        out[rownames(y), colnames(y)] <- y
+        out[rn0, cn0] <- x[rn0, cn0] + y[rn0, cn0]
+        out
+})
