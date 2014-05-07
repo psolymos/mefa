f7e6cb1771403123cfa765a25462855860d3eecc
psolymos <psolymos@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-05-07 15:53:36 +0000

    arguments added

--- pkg/mefa4/R/mefa4utilities.R
+++ pkg/mefa4/R/mefa4utilities.R
@@ -79,9 +79,26 @@ function(..., collapse = NULL)
 
 ## clear up species names
 nameAlnum <- 
-function(x) {
+function(x, capitalize=c("asis", "first", "none", "all", "mixed"), collapse=" ") 
+{
+    capitalize <- match.arg(capitalize)
+    .capwords <- function(x) {
+        s <- strsplit(x, " ")[[1]]
+        paste(toupper(substring(s, 1, 1)), substring(s, 2),
+              sep = "", collapse = " ")
+    }
+    capwords <- function(x) sapply(x, .capwords)
+    if (capitalize == "first") {
+        x <- tolower(x)
+        capitalize <- "mixed"
+    }
+    f <- switch(capitalize,
+        "asis"=function(x) return(x), 
+        "mixed"=capwords, 
+        "none"=tolower, 
+        "all"=toupper)
     sapply(x, function(z) {
-        paste0(strsplit(gsub("[^[:alnum:] ]", "", z), " +")[[1]], collapse="")
-    }, USE.NAMES = FALSE)
+        paste0(f(strsplit(gsub("[^[:alnum:] ]", "", z), " +")[[1]]), collapse=collapse)
+    }, USE.NAMES = !is.null(names(x)))
 }
 
